3929431265b46250b0ea17c653d333789a7d0ae3
Leonard Campanello <38050581+ljcamp1624@users.noreply.github.com>
2024-08-26 22:14:30 -0400

    Train model using DQN framework
    Train model using DQN framework.

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ Created on Sat Aug 24 20:44:32 2024
 @author: Lenny
 """
 
-#%% Start game
-player = Player()
+#%% Set up player
+from player import *
+from tictactoe import *
+
+#%% Collect data
 
 import numpy as np
 
-def RunGame():
+def RunGame(player):
     game=TicTacToe()
     
     # play the game and store the moves
@@ -32,26 +35,76 @@ def RunGame():
         data.append([b, m, p])
         
     # assert the outcome
-    for i, d in enumerate(data):
-        vec = np.zeros((9, 1)) + 0.5        
-        if d[2] == game.outcome:
-            vec[d[1]] = 1
-        elif game.outcome != 0.5:
-            vec[d[1]] = 0
+    for i, d in enumerate(data[:-2]):
+        vec = np.zeros((9, 1))
         data[i].append(vec)
+        
+    vec = np.zeros((9, 1))
+    vec[data[-2][1]] = -1
+    data[-2].append(vec)
+    
+    vec = np.zeros((9, 1))
+    vec[data[-1][1]] = 1
+    data[-1].append(vec)
         
     return data
 
 
-def RunTrials(trials=1):
+def RunGameTrials(player, num_games=1):
     data = []
-    for t in range(trials):
-        data += RunGame()
+    for t in range(num_games):
+        data += RunGame(player)
     return data
+
+
+def ExtractTrainingData(game_data):
+    # symmetry idx
+    lr_idx = [2,1,0,5,4,3,8,7,6]
+    ud_idx = [6,7,8,3,4,5,0,1,2]
+    diag_idx = [0,3,6,1,4,7,2,5,8]
+    odiag_idx = [8,5,2,7,4,1,6,3,0]
+    x = []
+    y = []
+    for v in game_data:
+        b = v[0]
+        t = v[3]
+        x.append(b)
+        x.append(np.fliplr(b))
+        x.append(np.flipud(b))
+        y.append(t)
+        y.append(t[lr_idx])
+        y.append(t[ud_idx])
+    return np.array(x), np.array(y)
     
-    
-    
-for i in range(100):
-    x = RunTrials(1)
-    print('-------------------------------------------------------------------------------------------------------------')
-    
+
+def RunTraining(player, num_games=1, num_repeats=1, drop_rate=0.0):
+    player.mode = 'Play'
+    game_data = RunGameTrials(player, num_games)
+    board_positions, move_outcomes = ExtractTrainingData(game_data)
+    player.player.fit(
+        board_positions,
+        move_outcomes,
+        batch_size = 50,
+        epochs = 50,
+        validation_split = 0.25,
+        )
+
+#%% Train the machine
+player = Player()
+for i in range(10):
+    RunTraining(player, num_games=10)
+
+#%% Play the machine
+player.mode = 'Play'
+game = TicTacToe()
+while not game.game_over:
+    game.PrintBoard()
+    move = input()
+    game.MakeMove(eval(move))
+    move = player.GetMove(game.board)
+    valid = False
+    for m in move:
+        pos = [m // 3, m % 3]
+        valid = game.MakeMove(pos)
+        if valid:
+            break
--- player.py
+++ player.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+import keras
 
 class Player():
     
@@ -13,36 +15,39 @@ class Player():
             self.T = 0.01
         
     def InitializePlayerModel(self):
-        import tensorflow as tf
-        import keras
         player = keras.Sequential()
         player.add(keras.Input(shape=(3, 3, 1))),
         player.add(keras.layers.Conv2D(1000, (3, 3))),
         player.add(keras.layers.Flatten()),
         player.add(keras.layers.Dense(100, activation='relu')),
-        player.add(keras.layers.Dense(100, activation='relu')),
-        player.add(keras.layers.Dense(9, activation='sigmoid')),
+        player.add(keras.layers.Dense(20, activation='relu')),
+        player.add(keras.layers.Dense(9, activation='relu')),
         player.build()
         player.compile(
             optimizer=keras.optimizers.Adam(learning_rate=1e-3),
             run_eagerly=True,
+            loss=keras.losses.MeanSquaredError(),
             )
         return player
 
     def GetMove(self, board):
-        import numpy as np
-        move = self.player.predict(np.expand_dims(board, 0).astype(float))[0]
-        p_func = np.sum(np.exp(-move/self.T))
-        move_proba = np.exp(-move/self.T) / p_func
-        cum_move_proba = np.cumsum(move_proba)
-        move_list = []
-        for i in range(9):
-            n = int(np.argmax(cum_move_proba > np.random.rand()))
-            move_list.append(n)
-            move_proba[n] = 0
-            if np.sum(move_proba) == 0:
-                break
-            move_proba = move_proba / np.sum(move_proba)
-            cum_move_proba = np.cumsum(move_proba)
+        move_q_score = self.player.predict(
+            np.expand_dims(board, 0).astype(float))[0]
+        x, y = np.where(board == 0)
+        idx = np.argsort(move_q_score[3*x + y])[::-1]
+        move_list = (3*x + y)[idx]
+        # p_func = np.sum(
+        #     np.exp(-move_q_score/self.T))
+        # move_proba = np.exp(-move_q_score/self.T) / p_func
+        # cum_move_proba = np.cumsum(move_proba)
+        # move_list = []
+        # for i in range(9):
+        #     n = int(np.argmax(cum_move_proba > np.random.rand()))
+        #     move_list.append(n)
+        #     move_proba[n] = 0
+        #     if np.sum(move_proba) == 0:
+        #         break
+        #     move_proba = move_proba / np.sum(move_proba)
+        #     cum_move_proba = np.cumsum(move_proba)
         return move_list
     
